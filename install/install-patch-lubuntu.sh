2800459410e6f1eb94d449ed402c6a0445c5975d
ptC7H12 <ptgit@live.com>
2015-01-08 19:00:44 +0100

    Add some modifications

--- install/install-patch-lubuntu.sh
+++ install/install-patch-lubuntu.sh
@@ -2,14 +2,14 @@
 
 #Patches provided by jogi36de [lemaker.org forum]
 
-echo ""
+echo 
 echo "/xbmc/filesystem/SMBDirectory.cpp wird gepatcht!"
-cp ./patches/xbmc-bp-patch6.patch ../../
+cp patches/xbmc-bp-patch6.patch ../../
 patch -p1 < ../../xbmc-bp-patch6.patch
 rm ../../xbmc-bp-patch6.patch
 
-echo ""
+echo 
 echo "/xbmc/filesystem/SmbFile.cpp wird gepatcht!"
-cp ./patches/xbmc-bp-patch7.patch ../../
+cp patches/xbmc-bp-patch7.patch ../../
 patch -p1 < ../../xbmc-bp-patch7.patch
 rm ../../xbmc-bp-patch7.patch
