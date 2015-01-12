76c8ba348b763a2b714e784d9b78650f6fb303b9
ptC7H12 <ptC7H12@users.noreply.github.com>
2015-01-12 23:17:04 +0100

    Update install-patch.sh

--- install/install-patch.sh
+++ install/install-patch.sh
@@ -7,10 +7,7 @@ if [ -d /touch ]
 	else
 		echo "Kalibrierdateien erstellen"
 		mkdir /touch
-		chmod 777 /touch -R
-		touch /touch/touchscreen_axis_calib
-		chmod 777 /touch/touchscreen_axis_calib
-		echo "calib_x_d=-21;calib_x_fact=0.658097686;calib_y_d=-50;calib_y_fact=0.408626561;swap_axes=0;click_confines=15" > /touch/touchscreen_axis_calib
+		chmod 666 /touch -R
 fi
 
 
